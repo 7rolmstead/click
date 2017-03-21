88e2d3244230b0a06777e52c3d66e5ec47f4af3f
7rolmstead <7rolmstead@stpatrick.org>
2017-03-21 10:48:48 -0500

    finished samecolor function

---
+++ click.js
@@ -0,0 +1,10 @@
+//declare samecolor() here
+function samecolor(hex){
+    let samecolor = doc.getElementById('samecolor')
+    div.background-color:#948318
+    for (var i = 0; i < samecolor.length; i++){
+        div.i opacity = Math.random
+    }
+
+}
+//declare diffcolor() here

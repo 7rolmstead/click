fb99d2a1bef04ea928626ed5a906de4ebea64a6a
7rolmstead <7rolmstead@stpatrick.org>
2017-03-23 10:50:22 -0500

    working on diffcolor function

--- click.js
+++ click.js
@@ -1,10 +1,25 @@
 //declare samecolor() here
 function samecolor(hex){
-    let samecolor = doc.getElementById('samecolor')
+    const doc = document;
+    let color = #196756
+    let divs = doc.querySelectorAll("samecolor div")
     div.background-color:#948318
     for (var i = 0; i < samecolor.length; i++){
-        div.i opacity = Math.random
+        divs[i].style.backgroundColor = color;
+        divs[i].opacity = Math.rand;
     }
-
 }
 //declare diffcolor() here
+function diffcolor(){
+    let color = 478649;
+    let complement = 761975;
+    let divs = doc.questySelectorAll("diffcolor dic")
+    for(var i = 0; i < divs.length; i++){
+        if i = %2 == 0{
+            divs[i].style.backgroundColor = color;
+        }
+        else{
+            
+        }
+    }
+}
